d8b5fcdfd33f7293b47a5a70351c971d2725853d
mat-hor <matt.hor.dev@gmail.com>
2025-03-09 14:24:48 +0100

    Added argv to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_char, sort_dict
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,7 +9,12 @@ def get_book_text(path_to_file):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     file_content = get_book_text(book_path)
     num_words = get_num_words(file_content)
     char_dict = get_num_char(file_content)
